7f6a9358b0d034d192d9281769d0ef08e9ea0f23
KariminK <103779342+KariminK@users.noreply.github.com>
2024-07-21 22:30:12 +0200

    added particles

---
+++ script.js
@@ -0,0 +1,23 @@
+/** @type {CanvasRenderingContext2D} */
+
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+let CANVAS_HEIGHT = (canvas.height = window.innerHeight);
+let CANVAS_WIDTH = (canvas.width = window.innerWidth);
+
+ctx.fillStyle = "white";
+
+const drawParticle = (x, y, size, ctx) => {
+  const random = Math.random() * 10;
+  ctx.fillRect(x + random, y, size, size);
+  setTimeout(() => {
+    ctx.clearRect(x + random - 1, y - 1, size + 5, size + 5);
+    if (size > 0) drawParticle(x, y + 5, size - 0.5, ctx);
+    else ctx.clearRect(x + random, y, size, size);
+  }, 50);
+};
+
+addEventListener("mousemove", (e) => {
+  drawParticle(e.clientX, e.clientY, 10, ctx);
+});
