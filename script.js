410550c6e7112963a781fcecf4cd16ea920ec2f3
KariminK <103779342+KariminK@users.noreply.github.com>
2025-12-23 22:47:49 +0100

    added experience section

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ addEventListener("mousemove", (ev) => {
   circle.style.top = `${y - 300}px`;
   circle.style.left = `${x - 300}px`;
 });
+
+const experienceSection = document.querySelector("#experience");
+const experienceNavigation = document.querySelector("#experience-nav");
+
+const projectsSection = document.querySelector("#projects");
+const projectsNavigation = document.querySelector("#projects-nav");
+
+projectsNavigation.addEventListener("click", () => {
+  projectsSection.classList.add("active");
+  projectsNavigation.classList.add("active");
+
+  experienceSection.classList.remove("active");
+  experienceNavigation.classList.remove("active");
+});
+
+experienceNavigation.addEventListener("click", () => {
+  experienceSection.classList.add("active");
+  experienceNavigation.classList.add("active");
+
+  projectsSection.classList.remove("active");
+  projectsNavigation.classList.remove("active");
+});
